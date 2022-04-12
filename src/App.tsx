f358b0179cb31a826902bb11fa1ec4fee93631de
Abhiseck Bhattacharya <24852420+iamalipe@users.noreply.github.com>
2022-04-12 06:20:00 +0530

    Basic ready and changing some config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+import ToggleTab from "./Components/ToggleTab";
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+`;
 
 function App() {
+  const names = ["Tab01", "Tab02", "Tab03", "Tab04"];
+  // const names = ["Tab01", "Tab02"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <ToggleTab names={names} id="Tab" />
+    </Div>
   );
 }
 
