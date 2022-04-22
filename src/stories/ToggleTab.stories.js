08de6c9c6b55bdc9ee5d76bc6194e21f6ef0c93c
Abhiseck Bhattacharya <24852420+iamalipe@users.noreply.github.com>
2022-04-22 22:05:17 +0530

    Change build mode

--- src/stories/ToggleTab.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import { ToggleTab } from "../components/ToggleTab";
-
-const stories = storiesOf("ToggleTab", module);
-stories.add("default", () => <ToggleTab />);
